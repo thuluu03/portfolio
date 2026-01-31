a6798cac0ecc4ba36d5feafa76580e5d01aacc1a
thuluu03 <98506834+thuluu03@users.noreply.github.com>
2026-01-31 16:44:09 -0500

    feat inter: project card alignment

--- src/components/ProjectCard/projectCard.tsx
+++ src/components/ProjectCard/projectCard.tsx
@@ -1,32 +1,26 @@
 import Tape from "./tape";
-import './projectCardStyle.css';
+import "./projectCardStyle.css";
 
 const ProjectCard = () => {
-    return (
-        <div>
-            <div className="container">
+  return (
+    <div className="project-card">
+      <div className="container">
+        <Tape color="pink" angle={30} />
 
-                <Tape color='pink' angle={30}/>
-
-                <div className="body">
-                    <div className="thumbnail">
-                        <div className="container">
-
-                        </div>
-                    </div>
-                    <div className="subheading">
-                        Lorem ipsum dolor sit
-                    </div>
-                    <div className="text">
-                        Lorem ipsum dolor sit amet consectetur adipiscing elit 
-                        quisque faucibus ex sapien vitae pellentesque sem 
-                        placerat in id cursus mi pretium tellus duis convallis 
-                        tempus.
-                    </div>
-                </div>
+        <div className="body">
+          <figure className="thumbnail"></figure>
+          <div className="text-wrapper">
+            <header className="subheading">Lorem ipsum dolor sit</header>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit quisque
+              faucibus ex sapien vitae pellentesque sem placerat in id cursus mi
+              pretium tellus duis convallis tempus.
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProjectCard;
--- src/components/ProjectCard/tape.tsx
+++ src/components/ProjectCard/tape.tsx
@@ -1,9 +1,12 @@
+import "./tapeStyle.css"
+
 const Tape = ({ color, angle }: { color: string; angle: number }) => {
   return (
-    <div>
+    <div className="tape">
       <div
         className={`${color ? color : "white"} + ${angle ? angle : "30"}`}
-      ></div>
+      >
+      </div>
     </div>
   );
 };
