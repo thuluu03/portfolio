4f8f548c7323eb51f25183cc5ba37ad72e10ce6a
thuluu03 <98506834+thuluu03@users.noreply.github.com>
2026-02-05 23:23:37 -0500

    feat: detaisl object

---
+++ public/user.js
@@ -0,0 +1,33 @@
+const details = {
+  projects: {
+    0: {
+      name: 'HOPE Inventory Management',
+      blurb:
+        'A proposed solution to streamline communication between' +
+        'inventory and outreach sectors in the Housing for People Everywhere Club.',
+      link: 'github.com',
+    },
+    1: {
+      name: 'TCP/IP',
+      blurb:
+        'An abstraction of TCP and IP link layers. Implemented with reference to the RFC protocol',
+      link: 'github.com',
+    },
+    2: {
+      name: 'Website redesign and implementation: Conflict Research and Security Studies Lab',
+      blurb: 'Club project to create a website from scratch for CRSSL.',
+      link: 'github.com',
+    },
+    3: {
+      name: '....',
+      blurb: '....',
+      link: 'github.com',
+    },
+  },
+  collection: {
+    trinkets: '',
+    hobbies: '',
+  },
+};
+
+export default details;
