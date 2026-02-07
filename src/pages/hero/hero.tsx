a728b8c39f74404d5d41a2d4e74dde46e4334e80
thuluu03 <98506834+thuluu03@users.noreply.github.com>
2026-02-07 10:51:04 -0500

    fix: hero grid

---
+++ src/pages/hero/hero.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './heroStyle.module.css';
+
+const Hero = () => {
+  return (
+    <div className={styles.heroContainer}>
+      <h1 className="heading">Hero</h1>
+
+      <div className={styles.heroBody}>
+        <div className={styles.textWrapper}>
+          <h1 className="heading blue">Hey, It's Thu!</h1>
+          <p>
+            Aspiring software engineer.
+            <br></br>
+            Ivy-league educated.
+          </p>
+        </div>
+        <div className={styles.image}>fjoiesfeji</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
